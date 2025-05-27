3437add074afc31ed4ed957964b0657fd2e46623
wangyiyan <achievement1993@gmail.com>
2025-05-27 13:01:07 +0800

    將漢堡圖給予mt-[10px]讓它下來一點

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -135,7 +135,7 @@ export default function Navbar({
             className="text-gray-700 focus:outline-none cursor-pointer mr-5"
           >
             {/* 漢堡圖 */}
-            <svg className="w-6 h-6 text-purple-700" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
+            <svg className="w-6 h-6 text-purple-700 mt-[10px]" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
